a5e9ffd7ef003886817cda8ef6bf390cdb123114
Sidddha <nikitaprudov@gmail.com>
2022-01-22 23:01:47 +0300

    modified

--- mcc_generated_files/include/pin_manager.h
+++ mcc_generated_files/include/pin_manager.h
@@ -36,89 +36,29 @@
 #include <avr/io.h>
 #include "port.h"
 
-//get/set PWM0 aliases
-#define PWM0_SetHigh() do { PORTD |= 0x20; } while(0)
-#define PWM0_SetLow() do { PORTD &= ~0x20; } while(0)
-#define PWM0_Toggle() do { PIND |= 0x20; } while(0)
-#define PWM0_GetValue() (PIND & (0x1 << 5))
-#define PWM0_SetDigitalInput() do { DDRD &= ~0x20; } while(0)
-#define PWM0_SetDigitalOutput() do { DDRD |= 0x20; } while(0)
-#define PWM0_SetPullUp() do { PORTD |= 0x20; } while(0)
-#define PWM0_ResetPullUp() do { PORTD &= ~0x20; } while(0)
-#define PWM0_SetInverted() do { PORTD_PIN5CTRL  |= PORT_INVEN_bm; } while(0)
-#define PWM0_ResetInverted() do { PORTD_PIN5CTRL  &= ~PORT_INVEN_bm; } while(0)
+//get/set PD5 aliases
+#define PD5_SetHigh() do { PORTD |= 0x20; } while(0)
+#define PD5_SetLow() do { PORTD &= ~0x20; } while(0)
+#define PD5_Toggle() do { PIND |= 0x20; } while(0)
+#define PD5_GetValue() (PIND & (0x1 << 5))
+#define PD5_SetDigitalInput() do { DDRD &= ~0x20; } while(0)
+#define PD5_SetDigitalOutput() do { DDRD |= 0x20; } while(0)
+#define PD5_SetPullUp() do { PORTD |= 0x20; } while(0)
+#define PD5_ResetPullUp() do { PORTD &= ~0x20; } while(0)
+#define PD5_SetInverted() do { PORTD_PIN5CTRL  |= PORT_INVEN_bm; } while(0)
+#define PD5_ResetInverted() do { PORTD_PIN5CTRL  &= ~PORT_INVEN_bm; } while(0)
 
-//get/set PC4_SDA aliases
-#define PC4_SDA_SetHigh() do { PORTC |= 0x10; } while(0)
-#define PC4_SDA_SetLow() do { PORTC &= ~0x10; } while(0)
-#define PC4_SDA_Toggle() do { PINC |= 0x10; } while(0)
-#define PC4_SDA_GetValue() (PINC & (0x1 << 4))
-#define PC4_SDA_SetDigitalInput() do { DDRC &= ~0x10; } while(0)
-#define PC4_SDA_SetDigitalOutput() do { DDRC |= 0x10; } while(0)
-#define PC4_SDA_SetPullUp() do { PORTC |= 0x10; } while(0)
-#define PC4_SDA_ResetPullUp() do { PORTC &= ~0x10; } while(0)
-#define PC4_SDA_SetInverted() do { PORTC_PIN4CTRL  |= PORT_INVEN_bm; } while(0)
-#define PC4_SDA_ResetInverted() do { PORTC_PIN4CTRL  &= ~PORT_INVEN_bm; } while(0)
-
-//get/set PWM1 aliases
-#define PWM1_SetHigh() do { PORTD |= 0x40; } while(0)
-#define PWM1_SetLow() do { PORTD &= ~0x40; } while(0)
-#define PWM1_Toggle() do { PIND |= 0x40; } while(0)
-#define PWM1_GetValue() (PIND & (0x1 << 6))
-#define PWM1_SetDigitalInput() do { DDRD &= ~0x40; } while(0)
-#define PWM1_SetDigitalOutput() do { DDRD |= 0x40; } while(0)
-#define PWM1_SetPullUp() do { PORTD |= 0x40; } while(0)
-#define PWM1_ResetPullUp() do { PORTD &= ~0x40; } while(0)
-#define PWM1_SetInverted() do { PORTD_PIN6CTRL  |= PORT_INVEN_bm; } while(0)
-#define PWM1_ResetInverted() do { PORTD_PIN6CTRL  &= ~PORT_INVEN_bm; } while(0)
-
-//get/set PC5_SCL aliases
-#define PC5_SCL_SetHigh() do { PORTC |= 0x20; } while(0)
-#define PC5_SCL_SetLow() do { PORTC &= ~0x20; } while(0)
-#define PC5_SCL_Toggle() do { PINC |= 0x20; } while(0)
-#define PC5_SCL_GetValue() (PINC & (0x1 << 5))
-#define PC5_SCL_SetDigitalInput() do { DDRC &= ~0x20; } while(0)
-#define PC5_SCL_SetDigitalOutput() do { DDRC |= 0x20; } while(0)
-#define PC5_SCL_SetPullUp() do { PORTC |= 0x20; } while(0)
-#define PC5_SCL_ResetPullUp() do { PORTC &= ~0x20; } while(0)
-#define PC5_SCL_SetInverted() do { PORTC_PIN5CTRL  |= PORT_INVEN_bm; } while(0)
-#define PC5_SCL_ResetInverted() do { PORTC_PIN5CTRL  &= ~PORT_INVEN_bm; } while(0)
-
-//get/set NTC0 aliases
-#define NTC0_SetHigh() do { PORTC |= 0x1; } while(0)
-#define NTC0_SetLow() do { PORTC &= ~0x1; } while(0)
-#define NTC0_Toggle() do { PINC |= 0x1; } while(0)
-#define NTC0_GetValue() (PINC & (0x1 << 0))
-#define NTC0_SetDigitalInput() do { DDRC &= ~0x1; } while(0)
-#define NTC0_SetDigitalOutput() do { DDRC |= 0x1; } while(0)
-#define NTC0_SetPullUp() do { PORTC |= 0x1; } while(0)
-#define NTC0_ResetPullUp() do { PORTC &= ~0x1; } while(0)
-#define NTC0_SetInverted() do { PORTC_PIN0CTRL  |= PORT_INVEN_bm; } while(0)
-#define NTC0_ResetInverted() do { PORTC_PIN0CTRL  &= ~PORT_INVEN_bm; } while(0)
-
-//get/set FREQ_CAPTURE aliases
-#define FREQ_CAPTURE_SetHigh() do { PORTB |= 0x1; } while(0)
-#define FREQ_CAPTURE_SetLow() do { PORTB &= ~0x1; } while(0)
-#define FREQ_CAPTURE_Toggle() do { PINB |= 0x1; } while(0)
-#define FREQ_CAPTURE_GetValue() (PINB & (0x1 << 0))
-#define FREQ_CAPTURE_SetDigitalInput() do { DDRB &= ~0x1; } while(0)
-#define FREQ_CAPTURE_SetDigitalOutput() do { DDRB |= 0x1; } while(0)
-#define FREQ_CAPTURE_SetPullUp() do { PORTB |= 0x1; } while(0)
-#define FREQ_CAPTURE_ResetPullUp() do { PORTB &= ~0x1; } while(0)
-#define FREQ_CAPTURE_SetInverted() do { PORTB_PIN0CTRL  |= PORT_INVEN_bm; } while(0)
-#define FREQ_CAPTURE_ResetInverted() do { PORTB_PIN0CTRL  &= ~PORT_INVEN_bm; } while(0)
-
-//get/set IO_PC1 aliases
-#define IO_PC1_SetHigh() do { PORTC |= 0x2; } while(0)
-#define IO_PC1_SetLow() do { PORTC &= ~0x2; } while(0)
-#define IO_PC1_Toggle() do { PINC |= 0x2; } while(0)
-#define IO_PC1_GetValue() (PINC & (0x1 << 1))
-#define IO_PC1_SetDigitalInput() do { DDRC &= ~0x2; } while(0)
-#define IO_PC1_SetDigitalOutput() do { DDRC |= 0x2; } while(0)
-#define IO_PC1_SetPullUp() do { PORTC |= 0x2; } while(0)
-#define IO_PC1_ResetPullUp() do { PORTC &= ~0x2; } while(0)
-#define IO_PC1_SetInverted() do { PORTC_PIN1CTRL  |= PORT_INVEN_bm; } while(0)
-#define IO_PC1_ResetInverted() do { PORTC_PIN1CTRL  &= ~PORT_INVEN_bm; } while(0)
+//get/set PD6 aliases
+#define PD6_SetHigh() do { PORTD |= 0x40; } while(0)
+#define PD6_SetLow() do { PORTD &= ~0x40; } while(0)
+#define PD6_Toggle() do { PIND |= 0x40; } while(0)
+#define PD6_GetValue() (PIND & (0x1 << 6))
+#define PD6_SetDigitalInput() do { DDRD &= ~0x40; } while(0)
+#define PD6_SetDigitalOutput() do { DDRD |= 0x40; } while(0)
+#define PD6_SetPullUp() do { PORTD |= 0x40; } while(0)
+#define PD6_ResetPullUp() do { PORTD &= ~0x40; } while(0)
+#define PD6_SetInverted() do { PORTD_PIN6CTRL  |= PORT_INVEN_bm; } while(0)
+#define PD6_ResetInverted() do { PORTD_PIN6CTRL  &= ~PORT_INVEN_bm; } while(0)
 
 void PIN_MANAGER_Initialize();
 #endif /* PINS_H_INCLUDED */
